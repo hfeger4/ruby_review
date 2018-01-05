f76a9dfaeec4baa28fe068b666f12a0aec1f658e
Howard Feger <hfeger4@gmail.com>
2018-01-05 13:49:10 -0800

    fix one_away to handle more test cases

--- CrackingTheCodingInterview/chapter1Strings.rb
+++ CrackingTheCodingInterview/chapter1Strings.rb
@@ -14,14 +14,33 @@ def is_unique(str)
   set1.length == str.length
 end
 
+
+def uniq_hash(str)
+  uniq = Hash.new 0
+
+end
+
 p is_unique("abcdee") == false
 p is_unique("abcde") == true
 
+# def check_permutation(str1,str2)
+#   return false if str1.gsub(/[^a-zA-Z]/,"").chars.sort != str2.gsub(/[^a-zA-Z]/,"").chars.sort
+#   true
+# end
+
 def check_permutation(str1,str2)
-  return false if str1.gsub(/[^a-zA-Z]/,"").chars.sort != str2.gsub(/[^a-zA-Z]/,"").chars.sort
+  hash = Hash.new 0
+  str1.chars.each do |let|
+    hash[let] += 1
+  end
+  str2.chars.each do |let|
+    hash[let] -= 1
+    return false if hash[let] < 0
+  end
   true
 end
 
+puts "CHECK PERMUTATION"
 p check_permutation("aca","aac")
 p check_permutation("aabeecd","beaeadc")
 
@@ -36,22 +55,59 @@ def palindrome_permutation(str)
   str.downcase.gsub(/[^a-z]/,"").chars.each do |letter|
     hash[letter] += 1
   end
-  hash.values.select{|v| v == 1}.length <= 1
+  hash.values.select{|v| v.odd?}.length >= 1
 end
 
+puts "PALINDROME PERMUTATION"
 p palindrome_permutation("Tact Coa")
+p palindrome_permutation("xhananahx")
 
 def one_away(str1, str2)
+  # return false if (str1.length - str2.length).abs > 1
   # if (str1.chars - str2.chars | str2.chars - str1.chars).length > 2
   #   return false
   # end
   # true
+
+  return false if (str1.length - str2.length).abs > 1
+
+  hash = Hash.new 0
+
+  str1.chars.each do |let|
+    hash[let] += 1
+  end
+
+  str2.chars.each do |let|
+    hash[let] -= 1
+  end
+
+  diff = 0
+
+  hash.values.each do |val|
+    diff += (val.to_f.abs/2)
+  end
+
+  return false if diff > 1.0
+  true
 end
 
+puts "ONE AWAY"
 p one_away("pale", "ple") == true
 p one_away("pales", "pale") == true
 p one_away("pale", "bale") == true
 p one_away("pale", "bae") == false
+p one_away('sdhg', 'ssghz') == false
+p one_away('coffee', 'coffe') == true
+p one_away('coffee', 'coffeee') == true
+p one_away('coffee', 'coffeex') == true
+p one_away('coffee', 'coffeexy') == false
+p one_away('aaxnn', 'aahnn') == true
+p one_away('aaxxnn', 'aahhnn') == false
+p one_away('ann', 'anh') == true
+p one_away('edg', 'dg') == true
+p one_away('palindromieee', 'ialindrompeee') == true
+p one_away('palindromieee', 'palidromiee') == false
+
 
 def str_compression(str)
   arr = []
