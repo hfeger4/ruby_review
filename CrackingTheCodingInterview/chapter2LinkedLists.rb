6ea4f4e8b519f4a54d4e16da5da64a85b8725b5b
Howard Feger <hfeger4@gmail.com>
2018-01-05 18:37:51 -0800

    add remove_dups to linked_list

--- CrackingTheCodingInterview/chapter2LinkedLists.rb
+++ CrackingTheCodingInterview/chapter2LinkedLists.rb
@@ -6,6 +6,31 @@
 require "Set"
 require_relative 'linked_list.rb'
 
+def remove_dups(linked_list)
+  hash = Hash.new
+  dups = []
+  linked_list.each do |node|
+    if !hash.values.include?(node.val)
+      hash[node.key] = node.val
+    else
+      dups << node.key
+    end
+  end
+  dups.each do |val|
+    linked_list.remove(val)
+  end
+  linked_list.to_s
+end
 
+linked_list = LinkedList.new
+linked_list.append(1,1)
+linked_list.append(2,2)
+linked_list.append(3,3)
+linked_list.append(4,1)
+linked_list.append(5,2)
+linked_list.append(6,2)
+linked_list.append(7,2)
 
-# Return Kth to Last: Implement an algorithm to  nd the kth to last element of a singly linked list.
+p remove_dups(linked_list)
+
+# Return Kth to Last: Implement an algorithm to  find the kth to last element of a singly linked list.
