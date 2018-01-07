1baca1315a1ae5a4c19dbcf1fcb87b654ba72ba3
Howard Feger <hfeger4@gmail.com>
2018-01-06 20:19:49 -0800

    add delete node question

--- CrackingTheCodingInterview/chapter2LinkedLists.rb
+++ CrackingTheCodingInterview/chapter2LinkedLists.rb
@@ -56,3 +56,11 @@ p  p1.key.to_s
 end
 
 kth_to_last(linked_list2, 3)
+
+
+# Delete Middle Node: Implement an algorithm to delete a node in the middle
+# (i.e., any node but the  rst and last node, not necessarily the exact middle) of a singly linked list,
+ # given only access to that node.
+# EXAMPLE
+# lnput:the node c from the linked lista->b->c->d->e->f
+# Result: nothing is returned, but the new linked list looks likea->b->d->e- >f
