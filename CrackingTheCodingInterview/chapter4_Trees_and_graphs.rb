f6780806d91450ef6eab5ed7350ef8f03205f06f
Howard Feger <hfeger4@gmail.com>
2018-01-16 23:21:00 -0800

    add minimal tree and list of depths questions

--- CrackingTheCodingInterview/chapter4_Trees_and_graphs.rb
+++ CrackingTheCodingInterview/chapter4_Trees_and_graphs.rb
@@ -1,3 +1,9 @@
 # Route Between Nodes:
 # Given a directed graph, design an algorithm to and out whether there is a
 # route between two nodes.
+
+# Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algo­
+# rithm to create a binary search tree with minimal height.
+
+# List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes
+# at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
