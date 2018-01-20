4253ae0e60b40a079e9be35d9b53c8ab1b8bcf72
Howard Feger <hfeger4@gmail.com>
2018-01-19 16:28:36 -0800

    add build order project

--- CrackingTheCodingInterview/chapter4_Trees_and_graphs.rb
+++ CrackingTheCodingInterview/chapter4_Trees_and_graphs.rb
@@ -14,5 +14,11 @@
 
 # Validate BST: Implement a function to check if a binary tree is a binary search tree.
 
-# Successor: Write an algorithm to  nd the "next" node (i.e., in-order successor) of a given node in a binary search tree. 
+# Successor: Write an algorithm to  nd the "next" node (i.e., in-order successor) of a given node in a binary search tree.
 # You may assume that each node has a link to its parent.
+
+# Build Order: You are given a list of projects and a list of dependencies (which is a list of pairs of projects, where the second project is dependent on the  rst project). All of a project's dependencies must be built before the project is. Find a build order that will allow the projects to be built. If there is no valid build order, return an error.
+# EXAMPLE
+# Input:
+# projects: a, b, c, d, e, f
+# dependencies: (a, d), (f, b), (b, d), (f, a), (d, c) Output: f, e, a, b, d, c
